82e70c970bd07a1fbb7013a4beed318a516052ef
Devinda <sdndvnd@gmail.com>
2025-12-14 13:22:04 +0530

    logical operators

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
 class Main{
+
     public static void main(String[] args){
         int b=5;
-        int res= b++ ;
-        int res2 =++b;
+        int a=6;
+        int x=7;
+        int y=8;
+
+        boolean res = a<b && x<y;
+        boolean res2 = a<b|| x<y;
 
         System.out.println(res);
         System.out.println(res2);
