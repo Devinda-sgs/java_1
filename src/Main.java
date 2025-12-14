7333b2b390ed633ee18a95b62a909ad21f822d1b
Devinda <sdndvnd@gmail.com>
2025-12-14 20:02:23 +0530

    switch stement old way

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,33 @@
 class Main{
 
     public static void main(String[] args){
-        int b=5;
-        int a=6;
-        int x=7;
-        int y=8;
+        int n= 1;
+        switch (n){
+            case 1:
+                System.out.print("its monday");
+                break;
+            case 2:
+                System.out.print("its tuesday");
+                break;
+            case 3:
+                System.out.print("its wednesday");
+                break;
+            case 4:
+                System.out.print("its thursday");
+                break;
+            case 5:
+                System.out.print("its friday");
+                break;
+            case 6:
+                System.out.print("its saturday");
+                break;
+            case 7:
+                System.out.print("its sunday");
+                break;
+            default:
+                System.out.print("enter valid number");
+        }
 
-        if (a<b && x<y){
-            System.out.println("hello");
-        }
-        else{
-            System.out.println("good");
-        }
-        if (a<b || x<y) {
-            System.out.println("bye");
-        } else if (a!=b) {
-            System.out.println("hi");
-
-        }
-        else{
-            System.out.println("go");
-        }
 
     }
 }
