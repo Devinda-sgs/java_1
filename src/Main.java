f0e670df66663a18861ab4f7a9e763c9095e330b
Devinda <sdndvnd@gmail.com>
2025-12-15 10:40:46 +0530

    while loop

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,19 @@
 class Main{
 
     public static void main(String[] args){
-        String day= "monday";
-        switch (day){
-            case "sunday" ->System.out.println("1");
-            case "monday" ->System.out.println("2");
-            case "wednesday" ->System.out.println("3");
 
-            default -> System.out.print("enter valid day");
-        };
-        int result;
+        int i=1;
 
-        String day1= "monday";
-        switch (day1){
-            case "sunday" ->result =1;
-            case "monday" ->result =2;
-            case "wednesday" -> result =3;
-
-            default -> result= 0;
-        };
-        System.out.print(result);
+        while(i<10){
+            System.out.println("hi"+i);
+            i++;
+            int j=1;
+            while(j<5){
+                System.out.println("bye"+j);
+                j++;
+            }
+        }
+        System.out.println("bye"+i );
 
 
     }
