6f607de6e04918ec934e7376e5a0f6f69bcaadc3
Devinda <sdndvnd@gmail.com>
2025-12-23 17:40:04 +0530

    ExpenseCalculator making, but I don't know how to use array in this case I like to add total value of expenses and average calculation. futhermore I should learn how class and objects are work.

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,37 @@
+import java.util.Scanner;
+
 class Main{
 
-    public static void main(String[] args){
-        calculator adding =new calculator();                //make object
-        int r  = adding.add(6,7);                           //pass pharameters and store it in variable r
-        System.out.println(r);                              //print result
-
+    public static void main(String args[]){
+        ExpenseCalculator calc =new ExpenseCalculator();
+        calc.getInput();
 
     }
+
 }
-class calculator{                                           //creaate class
-    public int add(int num1,int num2){                      //make method in it
-        int res =num1+num2;                                 //operation
-        return res;                                         //return output
+class ExpenseCalculator{
+    double[] expense;
+    public double getInput(){
+        Scanner sc =new Scanner(System.in);
+
+
+
+        System.out.println("Enter your name: ");
+        String name =sc.nextLine();
+        System.out.println("Enter numbers of days: ");
+        int days =sc.nextInt();
+
+        expense = new double[days];                             //create an array days mean size of array
+
+        for(int i=0;i<days;i++){
+            System.out.println("Enter your " +(i+1)+ " day expense: ");
+            expense[i] = sc.nextDouble();
+        }
+        return expense;
+
+
+
+
     }
+
 }
