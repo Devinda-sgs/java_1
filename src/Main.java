91c91a9b2667e5b9d2145e68169cec6617335531
Devinda <sdndvnd@gmail.com>
2025-12-23 19:12:10 +0530

    make class and object ,finaly call methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,25 @@
-import java.util.Scanner;
+class computer{
+    void playMusic(){
+        System.out.println("Music playing");
+    }
+    String getPen(int cost){
+        if (cost > 20){
+            String str ="Cost is: " +cost;
+            return str;
+        }
+        else {
+            String str = "you dont have enogh money";
+            return str;
+        }
+    }
+}
 
-class Main{
+public class Main{
     public static void main(String arg[]){
+        computer com1 = new computer();         //make com1 object
+        com1.playMusic();                       //call playMusic method
+        String stri=  com1.getPen(30);     //call getPen method and store retuned value stri variable
+        System.out.println(stri);               //print stris
 
     }
 }
