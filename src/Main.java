a53a985ebccec86e24e70573f8040a07e04de5dd
Devinda <sdndvnd@gmail.com>
2026-01-18 13:17:49 +0530

    maked array with objects and got data fromm it using normal for loop and enhanced for loop

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,33 @@
-
-
 public class Main{
     public static void main(String[] args) {
-        int arr[][] = new int[3][];             //jagged array
-        arr[0] = new int[2];
-        arr[1] = new int[3];
-        arr[2] = new int[4];
-        System.out.println(arr.length);
+        students student1 = new students();
+        student1.name = "Alice";
+        student1.age = 20;  
+        students student2 = new students();
+        student2.name = "Bob"; 
+        student2.age = 22;
+        student2.gender = "Male";
+        students student3 =new students();
+        student3.name = "Charlie";
+        student3.age = 19;     
+        student3.gender = "Male";
+
+        students stu[] = {student1, student2, student3};        //create studennts array
+       
+        for(int i=0;i<stu.length;i++){                          //method to print student details
+            System.out.println("Name: " + stu[i].name + ", Age: "   + stu[i].age + ", Gender: " + stu[i].gender);   
+        }
+        //useing enhanced for loop
+
+        for(students s : stu){
+            System.out.println("Name: " + s.name + ", Age: "   + s.age + ", Gender: " + s.gender);  
+        }
+    
+    
     }
+}
+class students{
+    String name;
+    int age;
+    String gender;
 }
