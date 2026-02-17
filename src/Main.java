12dc305409481626522e577d59490b188651787a
Devinda <sdndvnd@gmail.com>
2026-02-17 20:29:42 +0530

    maked edges
    edges class and constructor was make

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,15 @@
+class edge {
+    String to;
+    int weight;
+    public edge(String to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
 
+}
+
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
