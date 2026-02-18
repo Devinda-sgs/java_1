444c5f4f5ebe7dd1c16d0ea97e0f84bbd17932ce
Devinda <sdndvnd@gmail.com>
2026-02-18 09:18:47 +0530

    addcity and addroad methods are created

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+
+import java.util.*;
+
 class edge {
     String to;
     int weight;
@@ -5,6 +8,21 @@ class edge {
         this.to = to;
         this.weight = weight;
     }
+
+}
+class graph{
+    Map<String , List<edge>> map =new LinkedHashMap<>();
+
+    void addcity(String city){
+        map.putIfAbsent(city,new ArrayList<>());
+
+    }
+    void addroad(String from,String to,int distance){
+        map.get(from).add(new edge(to,distance));
+        map.get(to).add(new edge(from,distance));
+        
+    }
+
 
 }
 
