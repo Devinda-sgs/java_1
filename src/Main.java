7db69504df17fbf200f85f8236fb367f978fb255
Devinda <sdndvnd@gmail.com>
2026-02-18 09:39:48 +0530

    graph created

--- src/Main.java
+++ src/Main.java
@@ -20,7 +20,15 @@ class graph{
     void addroad(String from,String to,int distance){
         map.get(from).add(new edge(to,distance));
         map.get(to).add(new edge(from,distance));
-        
+
+    }
+    void printgraph(){
+        for(Map.Entry<String,List<edge>> entry: map.entrySet()){
+            System.out.println("city "+entry.getKey()+" is connected to:");
+            for(edge e : entry.getValue()){
+                System.out.println("  "+ e.to+" with distance "+ e.weight);
+            }
+        }
     }
 
 
@@ -28,6 +36,13 @@ class graph{
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        graph g = new graph();
+        g.addcity("A");
+        g.addcity("B");
+        g.addcity("C");
+        g.addroad("A","B",5);
+        g.addroad("B","C",10);
+        g.addroad("A","C",15);
+        g.printgraph(); 
     }
 }
